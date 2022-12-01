60f8b116f5d7fd6bab5f221395b5e4bde261fe22
Sturmannn <90060665+Sturmannn@users.noreply.github.com>
2022-12-01 22:54:02 +0300

    Update Serial_main.cpp

--- Serial_version/Serial_main.cpp
+++ Serial_version/Serial_main.cpp
@@ -17,25 +17,33 @@ clock_t start, finish;// замеры времени (засечки)
 float dt; // время работы блока кода (изменение времени)
 const int N = 20000000; // количество опционов для подсчёта
 
+const float invsqrt2 = sqrt(2.0f); // инварианты
+const float inv_square_sig = sig * sig;
+
 void GetOptionPrices(Option* opt)
 {
   float d1, d2, erf1, erf2;
   for (int i = 0; i < N; i++)
   {
-    opt[i].K = (float)rand() / (float)RAND_MAX * (250.0f - 50.0f) + 50.0f;
-    opt[i].s0 = (float)rand() / (float)RAND_MAX * (150.0f - 50.0f) + 50.0f; // Случайные числа в диапазоне
-    opt[i].T = (float)rand() / (float)RAND_MAX * (5.0f - 1.0f) + 1.0f;
-    d1 = log(opt[i].s0 / opt[i].K) + ((r + (sig * sig / 2.0f) * opt[i].T) / (sig * sqrt(opt[i].T)));
-    d2 = log(opt[i].s0 / opt[i].K) + ((r - (sig * sig / 2.0f) * opt[i].T) / (sig * sqrt(opt[i].T)));
-    erf1 = 0.5f + erf(d1 / sqrt(2.0f)) / 2.0f;
-    erf2 = 0.5f + erf(d2 / sqrt(2.0f)) / 2.0f;
-    opt[i].C = opt[i].s0 * erf1 - opt[i].K * exp(-1.0f * r * opt[i].T) * erf2;
+    d1 = log(opt[i].s0 / opt[i].K) + ((r + (inv_square_sig * 0.5f) * opt[i].T) / (sig * sqrt(opt[i].T)));
+    d2 = log(opt[i].s0 / opt[i].K) + ((r - (inv_square_sig * 0.5f) * opt[i].T) / (sig * sqrt(opt[i].T)));
+    erf1 = 0.5f + std::erf(d1 / invsqrt2) * 0.5f;
+    erf2 = 0.5f + std::erf(d2 / invsqrt2) * 0.5f;
+
+    opt[i].C = opt[i].s0 * erf1 - opt[i].K * exp((-1.0f) * r * opt[i].T) * erf2;
   }
 }
 
 int main(int argc, char* argv[])
 {
   Option* sample = new Option[N];
+  
+  for (int i = 0; i < N; i++)
+  {
+    sample[i].K = (float)rand() / (float)RAND_MAX * (250.0f - 50.0f) + 50.0f;
+    sample[i].s0 = (float)rand() / (float)RAND_MAX * (150.0f - 50.0f) + 50.0f; // Случайные числа в диапазоне
+    sample[i].T = (float)rand() / (float)RAND_MAX * (5.0f - 1.0f) + 1.0f;
+  }
 
   start = clock();
   GetOptionPrices(sample);
